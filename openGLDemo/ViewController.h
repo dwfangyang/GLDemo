8cdc95dfa115ac26495c1aeefc51e53c815effbd
jefffyangdis <fangyang@fangyangdemacbook-pro.local>
2016-09-04 14:39:17 +0800

    Initial Commit

---
+++ openGLDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  openGLDemo
+//
+//  Created by 方阳 on 16/9/4.
+//  Copyright © 2016年 dw_fangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ openGLDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  openGLDemo
+//
+//  Created by 方阳 on 16/9/4.
+//  Copyright © 2016年 dw_fangyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
